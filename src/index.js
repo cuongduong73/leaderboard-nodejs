d9804b9f043d66930b4e363a78cfe29894957176
cuongduong <duonghaccuong.vn@gmail.com>
2022-09-09 00:38:34 +0700

    Init 7

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ app.get('/api', (req, res) => {
 
 app.listen(3000, () => console.log('Listening on port 3000...'));
 
-            const hashed_password =
+const hashed_password =
     '$2b$12$J35ArGJy5qKISlpj5tzaaeMrMFZCGb/hFbsGSqUFg17ncTAHUYWtS';
 const plain_password = 'Chelsea@1995';
 bcrypt.compare(plain_password, hashed_password).then((res) => console.log(res));
