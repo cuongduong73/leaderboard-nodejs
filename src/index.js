ee3ec58ba580efbec7af15b85fdc8df1b9dde595
cuongduong <duonghaccuong.vn@gmail.com>
2022-09-09 00:17:30 +0700

    Init 1

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+
+app = express();
+
+app.get('/api', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(3000, () => console.log('Listening on port 3000...'));
+
+const hashed_password =
+    '$2b$12$J35ArGJy5qKISlpj5tzaaeMrMFZCGb/hFbsGSqUFg17ncTAHUYWtS';
+const plain_password = 'Chelsea@1995';
+bcrypt.compare(plain_password, hashed_password).then((res) => console.log(res));
